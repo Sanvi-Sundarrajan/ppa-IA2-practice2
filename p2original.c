e3c04d87befbca94f1a0ce4064035f11af3d3881
Sanvi Sundarrajan <93093295+Sanvi-Sundarrajan@users.noreply.github.com>
2022-02-08 14:43:12 +0000

    p2

--- p2original.c
+++ p2original.c
@@ -1,15 +1,45 @@
 /*Write a program to find if a triangle is scalene. A triangle is a scalene traingle if all the three sides of the triangle are not equal to each other.
 
- int input_side()
- int check_scalene(int a, int b, int c)
- void output(int a, int b, int c, int isscalene)
- int input_side()
- int check_scalene(int a, int b, int c)
- void output(int a, int b, int c, int isscalene)
+       int input_side()
+       int check_scalene(int a, int b, int c)
+       void output(int a, int b, int c, int isscalene)
 */
 #include <stdio.h>
+int input_side ()
+{
+  int n;
+  printf ("Enter the side of triangle\n");
+  scanf ("%d", &n);
+  return n;
+}
+int check_scalene (int a, int b, int c)
+{
+  if (a == b && b == c)
+    return 0;
+  else if (a == b || b == c || c == a)
+    return 1;
+  else
+    return 2;
+}
+void output (int a, int b, int c, int isscalene)
+{
+  if (isscalene == 2)
+    {
+      printf ("The traingle with sides %d,%d,%d is scalene\n", a, b, c);
+    }
+  else
+    {
+      printf ("The traingle with sides %d,%d,%d is not scalene\n", a, b, c);
+    }
+}
+int main ()
+{
+  int a, b, c, isscalene;
+  a = input_side ();
+  b = input_side ();
+  c = input_side ();
+  isscalene = check_scalene (a, b, c);
+  output (a, b, c, isscalene);
+  return 0;
 
-
-
-
-
+}
